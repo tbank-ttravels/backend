0ee2a55aed827c2a2c50bb25095754ed003e8a0f
Melnik I. G. <vgyvgt12@gmail.com>
2025-11-02 19:02:40 +0400

    init

---
+++ src/main/java/com/tbank/ttravels_backend/TtravelsBackendApplication.java
@@ -0,0 +1,13 @@
+package com.tbank.ttravels_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TtravelsBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TtravelsBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tbank/ttravels_backend/TtravelsBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tbank.ttravels_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TtravelsBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
