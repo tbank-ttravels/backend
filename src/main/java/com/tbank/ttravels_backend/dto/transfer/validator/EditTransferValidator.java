3f634f6948e1e632f0c19a0152fd630faa17d4ad
Бунтукова Светлана <nigihayami_kohaku@mail.ru>
2025-11-17 02:48:46 +0300

    корректное подк exception

--- src/main/java/com/tbank/ttravels_backend/dto/transfer/validator/EditTransferValidator.java
+++ src/main/java/com/tbank/ttravels_backend/dto/transfer/validator/EditTransferValidator.java
@@ -1,7 +1,7 @@
 package com.tbank.ttravels_backend.dto.transfer.validator;
 
 import com.tbank.ttravels_backend.dto.transfer.EditTransferRequest;
-import com.tbank.ttravels_backend.exception.ValidationException;
+import com.tbank.ttravels_backend.exception.Validation;
 
 import java.math.BigDecimal;
 
@@ -10,10 +10,10 @@ public class EditTransferValidator {
     public static void validate(EditTransferRequest request) {
 
         if (request.getSum() == null)
-            throw new ValidationException("Сумма обязательна");
+            throw new Validation("Сумма обязательна");
 
         if (request.getSum().compareTo(BigDecimal.ZERO) <= 0)
-            throw new ValidationException("Сумма должна быть больше 0");
+            throw new Validation("Сумма должна быть больше 0");
     }
 }
 
