2d2f63bc24d62ee864aac7be878fcb04e921ae1d
Victor Tikhonov <victor.tix777@mail.ru>
2025-11-26 10:02:08 +0400

    Добавлен swagger на ExpenseController и AnalyticExpenseController. Также на соответствующие ДТО
    - Переименовал AnalyticsController на AnalyticExpenseController

--- src/main/java/com/tbank/ttravels_backend/dto/expense_analytics/TravelExpenseAnalyticsDTO.java
+++ src/main/java/com/tbank/ttravels_backend/dto/expense_analytics/TravelExpenseAnalyticsDTO.java
@@ -1,6 +1,8 @@
 package com.tbank.ttravels_backend.dto.expense_analytics;
 
 
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.*;
 
 import java.math.BigDecimal;
@@ -11,11 +13,17 @@ import java.util.List;
 @NoArgsConstructor
 @Getter
 @Setter
+@Schema(description = "Аналитика расходов по поездке")
 public class TravelExpenseAnalyticsDTO {
 
     // Общая сумма всех расходов по поездке
+    @Schema(description = "Общая сумма всех расходов по поездке", example = "2000.0")
     private BigDecimal totalAmount;
 
     // Аналитика по категориям
+    @ArraySchema(
+            arraySchema = @Schema(description = "Аналитика по категориям"),
+            schema = @Schema(implementation = CategoryAnalyticsResponseDTO.class)
+    )
     private List<CategoryAnalyticsResponseDTO> categories;
 }
