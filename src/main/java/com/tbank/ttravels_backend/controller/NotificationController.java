d032befffffda15704338c078605968baaa0e653
Kirill <mr.kirya.novikov.2014@gmail.com>
2025-12-14 04:13:11 +0400

    fixed NotificationController

--- src/main/java/com/tbank/ttravels_backend/controller/NotificationController.java
+++ src/main/java/com/tbank/ttravels_backend/controller/NotificationController.java
@@ -1,12 +1,14 @@
 package com.tbank.ttravels_backend.controller;
 
-import com.tbank.ttravels_backend.dto.notification.PushNotificationRequest;
-import com.tbank.ttravels_backend.dto.notification.PushNotificationResponse;
 import com.tbank.ttravels_backend.enums.Platform;
 import com.tbank.ttravels_backend.service.NotificationService;
+import com.tbank.ttravels_backend.security.UserPrincipal;
 import lombok.extern.slf4j.Slf4j;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -18,20 +20,22 @@ public class NotificationController {
     private NotificationService notificationService;
 
     @PostMapping("/register-token")
-    public ResponseEntity<?> registerToken(
-            @RequestParam Long userId,
+    @PreAuthorize("isAuthenticated()")
+    public ResponseEntity<Void> registerToken(
+            @AuthenticationPrincipal UserPrincipal user,
             @RequestParam String token,
             @RequestParam(defaultValue = "ANDROID") Platform platform) {
 
-        notificationService.registerDeviceToken(userId, token, platform);
-        log.info("Токен зарегистрирован для пользователя {}", userId);
-        return ResponseEntity.ok("Токен успешно зарегистрирован");
+        notificationService.registerDeviceToken(user.getId(), token, platform);
+        log.info("Токен зарегистрирован для пользователя {}", user.getId());
+        return ResponseEntity.ok().build();
     }
 
     @PostMapping("/unregister-token")
-    public ResponseEntity<?> unregisterToken(@RequestParam String token) {
+    @PreAuthorize("isAuthenticated()")
+    public ResponseEntity<Void> unregisterToken(@RequestParam String token) {
         notificationService.unregisterDeviceToken(token);
         log.info("Токен удалён");
-        return ResponseEntity.ok("Токен успешно удалён");
+        return ResponseEntity.ok().build();
     }
 }
